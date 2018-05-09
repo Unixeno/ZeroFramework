1bf58bed4a57703d7257fe1dcc8da928de8959f4
yangwang <li1397937810@gmail.com>
2018-05-09 11:11:29 +0800

    use static class to store and manage configurations

---
+++ zero/Config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Unixeno
+ * Date: 2018/5/9
+ * Time: 10:34
+ */
+
+namespace zero;
+
+
+class Config {
+    private static $config = [];
+
+    public static function get($name) {
+        if (isset(self::$config[$name])) {
+            return self::$config;
+        }
+    }
+
+    public static function set($name, $value) {
+        self::$config[$name] = $value;
+    }
+
+    public static function setArray($array) {
+        foreach ($array as $name => $value) {
+            self::$config[$name] = $value;
+        }
+    }
+}
